23207fab07a3974f55d8f713c00deae301750ed9
Prerana <dahiwadeprerana@gmail.com>
2022-11-14 22:44:00 +0530

    [prerana]Add:InventoryData

---
+++ OopsProject/InventoryData/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsProject.InventoryData
+{
+    internal class Inventory
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+
+        public double Price { get; set; }
+    }
+}
---
+++ OopsProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using OopsProject.InventoryData;
+
+namespace OopsConceptProject
+{
+    internal class Program
+    {
+
+        static string inventoryDataFilePath = @"D:\217.NetBatch\Ooops_Project\OopsProject\InventoryData\Inventory.json";
+
+        static void Main(string[] args)
+        {
+            InventoryManagement inventory = new InventoryManagement();
+            inventory.ReadJsonFile(inventoryDataFilePath);
+
+
+        }
+    }
+}
